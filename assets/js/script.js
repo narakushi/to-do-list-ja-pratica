e65bdc6872e50a6948214e0c9763641ead99947c
narakushi <taina.santana1305@gmail.com>
2025-03-15 19:07:37 -0300

    criando função para capturar e adicionar tarefas a lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,49 @@ const toDoList = () => {
     inputTask.placeholder = "Adicionar uma tarefa (para adicionar pressione [Enter])";
   });
 
-/** adicionando tarefas */
+  /** adicionando tarefas */
 
+  const appendTaskContainer = (task) => {
+    if (task) {
+      const containerBodyContent = document.querySelector(".container__body-content");
+      containerBodyContent.innerHTML += ` 
+            <div class="single-task">
+              <div class="single-task__task">
+                <input id="checkbox1" type="checkbox" class="checkbox"/>
+                <label for="checkbox1">${task}</label>
+              </div>
+              <div class="single-task__icons">
+                <button title="Editar tarefa">
+                  <span class="fa-solid fa-pen-to-square"></span>
+                </button>
+                <button title="Excluir tarefa">
+                  <span class="fa-solid fa-trash-can"></span>
+                </button>
+              </div>
+            </div>`
+    }
+
+  }
+
+  // capturando tarefa
+
+  const captureTask = () => {
+    const task = inputTask.value;
+    appendTaskContainer(task);
+    inputTask.value = '';
+  }
+
+  addEventListener("click", (e) => {
+    if (e.target.classList.contains("btnAdd")) {
+      captureTask();
+    }
+  })
+
+  addEventListener("keydown", (e) => {
+    if (e.key === 'Enter') {
+      captureTask();
+    }
+  })
 
 }
 
