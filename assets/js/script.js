c130bceea64e262497c9366ad722180c288f5e3a
narakushi <taina.santana1305@gmail.com>
2025-03-15 15:55:12 -0300

    Criando função to-do list e alterando font-size para mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
-/** Alterando placeholder conforme tamanho da tela */
 
-const inputTask = document.querySelector(".input_task");
 
-addEventListener("resize", (e) => {
-  const windowViewport = window.visualViewport.width;
-  if(windowViewport <= 425){
-    inputTask.placeholder = "Adicionar uma tarefa";
-    return
-  }
-  inputTask.placeholder = "Adicionar uma tarefa (para adicionar pressione [Enter])";
-});
+const toDoList = () => {
+  /** Alterando placeholder conforme tamanho da tela */
+
+  const inputTask = document.querySelector(".input_task");
+
+  addEventListener("resize", (e) => {
+    const windowViewport = window.visualViewport.width;
+    if (windowViewport <= 425) {
+      inputTask.placeholder = "Adicionar uma tarefa";
+      return
+    }
+    inputTask.placeholder = "Adicionar uma tarefa (para adicionar pressione [Enter])";
+  });
+
+/** adicionando tarefas */
+
+
+}
+
+toDoList();
 
 
